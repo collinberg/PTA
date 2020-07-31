6b32af926f501b5a2099de62fb484775aeb37328
Collin B <berg.collin@gmail.com>
2020-07-31 12:20:31 -0400

    Events Template

---
+++ resources/views/event.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app')
+
+@section('content')
+  @include('partials.page-header')
+
+
+  <div id="tribe-events-pg-template">
+
+    @while(have_posts()) @php the_post() @endphp
+
+    @php the_content() @endphp
+
+    @endwhile
+    
+  </div> <!-- #tribe-events-pg-template -->
+@endsection
