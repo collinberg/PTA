be1cc0ad657705d3f1070e8c4fe71003f979996d
Collin B <berg.collin@gmail.com>
2020-08-06 13:21:42 -0400

    adjusted spacing between posts and headers

--- resources/views/partials/content.blade.php
+++ resources/views/partials/content.blade.php
@@ -1,9 +1,11 @@
-<article @php post_class('flex flex-col lg:w-1/2 lg:pr-5 w-full mb-5 lg:mb-10 items-stretch') @endphp>
-  <div class='w-fulllg:pb-5 bg-grey'>
-      <img src="@asset('images/placeholder.jpg')">
+<article @php post_class('article-post flex flex-col mb-5 lg:mb-10 items-stretch') @endphp>
+  <div class='w-full mb-2'>
+    <a href="{{ get_permalink() }}" class='border-none'>
+      <img src="{{ \App\asset_path('images/placeholder.jpg') }}" alt="{!! get_the_title() !!}" class='rounded-sm'>
+    </a>
   </div>
   <div class='w-full'>
-    <h2 class="entry-title text-4xl mb-0 font-bold leading-tight">
+    <h2 class="entry-title text-lg lg:text-3xl mb-0 font-bold leading-none mb-2">
       <a href="{{ get_permalink() }}" class='text-grey-darkest border-none hover:text-primary'>{!! get_the_title() !!}</a>
     </h2>
     @include('partials/entry-meta')
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,4 +1,4 @@
-<header class="banner absolute w-full" aria-label="Main Navigation">
+<header class="banner bg-primary w-full" aria-label="Main Navigation">
   <div class="px-8 mx-auto">
     <div class='flex items-start'>
       <div class='w-full flex row items-center pt-5'>
@@ -13,9 +13,9 @@
           @endif
         </nav>
       </div>
-      <div class='pt-5 flex w-1/5 hidden lg:block'>
+      <div class='pt-5 w-1/5 hidden lg:flex row justify-end'>
         <a href="/donate" class='btn btn-secondary'>Donate</a>
-        <button id='searchToggle' class="desktop-search-toggle lg:pl-2" data-search-toggle="closed" aria-label="Search Button">
+        <button id='searchToggle' class="desktop-search-toggle lg:ml-2 p-3" data-search-toggle="closed" aria-label="Search Button">
             <svg class="svg-icon mx-auto block search-icon" aria-hidden="true" role="img" focusable="false" width="23" height="23" viewBox="0 0 23 23">
               <path d="M38.710696,48.0601792 L43,52.3494831 L41.3494831,54 L37.0601792,49.710696 C35.2632422,51.1481185 32.9839107,52.0076499 30.5038249,52.0076499 C24.7027226,52.0076499 20,47.3049272 20,41.5038249 C20,35.7027226 24.7027226,31 30.5038249,31 C36.3049272,31 41.0076499,35.7027226 41.0076499,41.5038249 C41.0076499,43.9839107 40.1481185,46.2632422 38.710696,48.0601792 Z M36.3875844,47.1716785 C37.8030221,45.7026647 38.6734666,43.7048964 38.6734666,41.5038249 C38.6734666,36.9918565 35.0157934,33.3341833 30.5038249,33.3341833 C25.9918565,33.3341833 22.3341833,36.9918565 22.3341833,41.5038249 C22.3341833,46.0157934 25.9918565,49.6734666 30.5038249,49.6734666 C32.7048964,49.6734666 34.7026647,48.8030221 36.1716785,47.3875844 C36.2023931,47.347638 36.2360451,47.3092237 36.2726343,47.2726343 C36.3092237,47.2360451 36.347638,47.2023931 36.3875844,47.1716785 Z" transform="translate(-20 -31)"></path>
             </svg>
@@ -34,8 +34,8 @@
   </div>
 </header>
 <!-- Search Bar -->
-<div class='search-modal flex flex-wrap justify-center flex-col z-50'>
-  <div class='search-wrap'>
+<div class='search-modal flex-wrap justify-center flex-col z-50'>
+  <div class='px-8 mx-auto lg:w-3/4'>
     @include('partials.searchform')
   </div>
 </div>
