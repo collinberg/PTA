b0ac071edc1c74bdd0f0a9896b8d42d8873ffca3
Collin B <berg.collin@gmail.com>
2020-08-21 19:01:36 -0400

    Fixed Search results

--- resources/views/partials/content-search.blade.php
+++ resources/views/partials/content-search.blade.php
@@ -1,11 +1,24 @@
-<article @php post_class() @endphp>
+<article @php post_class('bg-white mb-5 lg:mb-10 lg:p-10 p-5') @endphp>
   <header>
-    <h2 class="entry-title"><a href="{{ get_permalink() }}">{!! get_the_title() !!}</a></h2>
+    <h2 class="entry-title font-bold text-2xl"><a href="{{ get_permalink() }}">{!! get_the_title() !!}</a></h2>
     @if (get_post_type() === 'post')
       @include('partials/entry-meta')
     @endif
   </header>
   <div class="entry-summary">
     @php the_excerpt() @endphp
+  </div>
+  <div class='post-type-tag mt-10'>
+    <span class='font-bold text-sm opacity-50'>
+      @if (get_post_type() === 'post')
+        <i class="fas fa-newspaper"></i> news
+      @elseif(get_post_type() == 'tribe_events')
+        <i class='fas fa-calendar'></i> Events
+      @elseif(get_post_type() == 'page')
+        <i class='fas fa-file'></i> Page
+        @else
+        {{get_post_type() }}
+      @endif
+    </span>
   </div>
 </article>
--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -2,17 +2,23 @@
 
 @section('content')
   @include('partials.page-header')
+  <div class="wrap container mb-10" role="document">
+    <div class="content flex flex-wrap">
+      <main class="main w-full pt-10">
+        @if (!have_posts())
+          <div class="alert alert-warning">
+            {{ __('Sorry, no results were found.', 'sage') }}
+          </div>
+          {!! get_search_form(false) !!}
+        @endif
 
-  @if (!have_posts())
-    <div class="alert alert-warning">
-      {{ __('Sorry, no results were found.', 'sage') }}
-    </div>
-    {!! get_search_form(false) !!}
-  @endif
+        @while(have_posts()) @php the_post() @endphp
+          @include('partials.content-search')
+        @endwhile
 
-  @while(have_posts()) @php the_post() @endphp
-    @include('partials.content-search')
-  @endwhile
+        {!! get_the_posts_navigation() !!}
+    </main>
+  </div>
+</div>
 
-  {!! get_the_posts_navigation() !!}
 @endsection
