663bb8da17545b7e020afa2b8268d4b4d23be9db
Collin B <berg.collin@gmail.com>
2020-08-28 16:10:40 -0400

    Mobile CSS fixes

--- resources/views/partials/page-header.blade.php
+++ resources/views/partials/page-header.blade.php
@@ -1,5 +1,5 @@
-<div class="page-header h-64 flex flex-col-reverse pb-4">
+<div class="page-header h-24 lg:h-64 flex flex-col-reverse pb-4">
   <div class='container'>
-    <h1 class='text-3xl lg:text-6xl text-white font-bold lowercase'>{!! App::title() !!}</h1>
+    <h1 class='text-4xl lg:text-6xl text-white font-bold lowercase'>{!! App::title() !!}</h1>
   </div>
 </div>
