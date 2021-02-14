2384d2ae6295a42bbc596e898fb2785ec7b767d1
collinberg <berg.collin@gmail.com>
2021-02-14 14:04:37 -0500

    Updated the Widths for the header

--- resources/views/partials/page-header.blade.php
+++ resources/views/partials/page-header.blade.php
@@ -1,5 +1,7 @@
 <div class="page-header h-24 lg:h-64 flex flex-col-reverse pb-4">
   <div class='container'>
-    <h1 class='text-4xl lg:text-6xl text-white font-bold lowercase'>{!! App::title() !!}</h1>
+    <div class='lg:w-4/5 lg:mx-auto'>
+      <h1 class='text-4xl lg:text-6xl text-white font-bold lowercase'>{!! App::title() !!}</h1>
+    </div>
   </div>
 </div>
