80bea73624493488a84408318f6373b73277e0bf
collinberg <berg.collin@gmail.com>
2021-02-16 18:15:02 -0500

    Special Function for image srcset

--- app/setup.php
+++ app/setup.php
@@ -561,3 +561,20 @@ add_action( 'admin_enqueue_scripts',  __NAMESPACE__ .'\\add_admin_styles' );
 function add_admin_styles() {
   wp_enqueue_style('admin-styles', get_template_directory_uri().'/admin-styles.css');
 }
+
+function awesome_acf_responsive_image($image_id,$image_size,$max_width){
+
+	// check the image ID is not blank
+	if($image_id != '') {
+
+		// set the default src image size
+		$image_src = wp_get_attachment_image_url( $image_id, $image_size );
+
+		// set the srcset with various image sizes
+		$image_srcset = wp_get_attachment_image_srcset( $image_id, $image_size );
+
+		// generate the markup for the responsive image
+		echo 'src="'.$image_src.'" srcset="'.$image_srcset.'" sizes="(max-width: '.$max_width.') 100vw, '.$max_width.'"';
+
+	}
+}
